6df5f9be12f821c259f0e8a2b84c114e5346e7d8
savilard <vitaliyantonoff@gmail.com>
2024-09-18 11:51:18 +0500

    Added the appearance of stars one by one, not synchronously

--- main.py
+++ main.py
@@ -40,6 +40,9 @@ def draw(canvas):
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
+        for _ in range(random.randint(1, 20)):
+            await asyncio.sleep(0)
+
         canvas.addstr(row, column, symbol, curses.A_DIM)
         for _ in range(20):
             await asyncio.sleep(0)
